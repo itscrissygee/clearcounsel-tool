b8d31f96cac25817b09b138b6bf8be6403579b50
Crissy Gee <emailcrissygee@gmail.com>
2026-07-08 11:59:29 -0500

    Delete clearcounsel-tool directory

--- clearcounsel-tool/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import ClearCounsel from './ClearCounsel'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ClearCounsel />
-  </React.StrictMode>
-)
